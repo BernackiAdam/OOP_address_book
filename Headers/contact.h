217aee0fcc994f0143808758fd6ccab9e6dc8594
Adam Bernacki <a.bernacki99@gmail.com>
2024-07-01 17:41:35 +0200

    added contact files

---
+++ Headers/contact.h
@@ -0,0 +1,17 @@
+#ifndef CONTACT_H
+#define CONTACT_H
+#include <iostream>
+using namespace std;
+
+class Contact{
+    int contactId;
+    int userId;
+    string name;
+    string surname;
+    string email;
+    string nrTel;
+    string address;
+};
+
+#endif    
+    
